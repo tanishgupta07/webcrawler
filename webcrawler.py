7168183e494abed7977f99280a9a76215a040d66
tanishgupta07 <37222553+tanishgupta07@users.noreply.github.com>
2018-12-21 15:48:32 +0530

    Update webcrawler.py

--- webcrawler.py
+++ webcrawler.py
@@ -16,7 +16,7 @@ def crawl(i, url):
         nextLink = link.get('href')
         #print(nextLink)
         try:
-            if "https://apolloclinics.in/" in nextLink and nextLink not in urlList:
+            if "https://example.in/" in nextLink and nextLink not in urlList:
                 urlList.append(nextLink)
                 newAdditions.append(nextLink)
                 file.write(nextLink + "\n")
@@ -37,8 +37,9 @@ def main():
     while i < len(urlList):
         crawl(i, urlList[i])
         i += 1
-
-file = open("tree_apolloclinics_all-packages.txt", "w")
-urlList = ["https://apolloclinics.in/all-packages/"]
+#name of the file in which list is stored
+file = open("textFile.txt", "w")
+#url of the website that is to be crawled
+urlList = ["https://testsite.in"]
 
 main()
